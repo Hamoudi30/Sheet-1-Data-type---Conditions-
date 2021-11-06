5a4ebb94176c80e652a72e4fe822e34b748923d4
Hamoudi30 <64968485+Hamoudi30@users.noreply.github.com>
2021-11-06 14:08:20 +0200

    Create C.cpp

---
+++ C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+  long long x, y;
+  cin >> x >> y;
+  cout << x << " + " << y << " = " << x + y << '\n';
+  cout << x << " * " << y << " = " << x * y << '\n';
+  cout << x << " - " << y << " = " << x - y << '\n';
+  return 0;
+}
