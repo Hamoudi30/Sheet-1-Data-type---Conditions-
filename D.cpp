bb1f70bbb319f007b8754e0c506b2ecd885bfd57
Hamoudi30 <64968485+Hamoudi30@users.noreply.github.com>
2021-11-06 14:08:56 +0200

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+  long long a, b, c, d, x, y;
+  cin >> a >> b >> c >> d;
+  x = a * b;
+  y = c * d;
+  cout << "Difference = " << x - y << endl;
+  return 0;
+}
