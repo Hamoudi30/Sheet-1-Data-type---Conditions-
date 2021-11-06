4ae83866ed0489b6e0567d2cac7531fe0d1c7641
Hamoudi30 <64968485+Hamoudi30@users.noreply.github.com>
2021-11-06 14:20:57 +0200

    Create S.cpp

---
+++ S.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+const double pi = 3.141592653;
+int main () {
+  float a;
+  cin >> a;
+  if (a >= 0 && a <= 25) {
+    cout << "Interval [0,25]" << endl;
+  } else if (a > 25 && a <= 50) {
+    cout << "Interval (25,50]" << endl;
+  } else if (a > 50 && a <= 75) {
+    cout << "Interval (50,75]" << endl;
+  } else if (a > 75 && a <= 100) {
+    cout << "Interval (75,100]" << endl;
+  } else {
+    cout << "Out of Intervals" << endl;
+  }
+  return 0;
+}
