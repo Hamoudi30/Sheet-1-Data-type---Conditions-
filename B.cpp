743a8c11bff19cb64b3ef11e7086925f9ed7b9ac
Hamoudi30 <64968485+Hamoudi30@users.noreply.github.com>
2021-11-06 14:07:32 +0200

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+  int x = 0;
+  cin >> x;
+  long long y;
+  cin >> y;
+  char n;
+  cin >> n;
+  float m;
+  cin >> m;
+  double h;
+  cin >> h;
+  cout << x << endl << y << endl << n << endl << m << endl << h << endl;
+  return 0;
+}
